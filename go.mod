833cc5bedd9e15a96b5dae2160f051a4f33bcd4b
Jared <jared@jaredbeckham.com>
2019-05-05 17:10:51 +0000

    Bootstrap influx client, set up .yml parsing

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/jtbeckha/linux_metrics
+
+go 1.12
+
+require (
+	github.com/influxdata/influxdb-client-go v0.0.1
+	gopkg.in/yaml.v2 v2.2.2
+)
--- linux_metrics.go
+++ linux_metrics.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"github.com/influxdata/influxdb-client-go"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Environment 	string 	`yaml:"environment"`
+	InfluxAddress 	string 	`yaml:"influxAddress"`
+	InfluxToken		string	`yaml:"influxToken"`
+}
 
 func main() {
-	fmt.Println("test")
+	configYml, err := ioutil.ReadFile("config.yml")
+	if err != nil {
+		panic(err)
+	}
+
+	conf := &Config{}
+	if err := yaml.Unmarshal(configYml, conf); err != nil {
+		panic(err)
+	}
+
+	influx, err := influxdb.New(
+		nil, influxdb.WithAddress(conf.InfluxAddress), influxdb.WithToken(conf.InfluxToken))
+	if err != nil {
+		panic(err)
+	}
+
+	hostname, err := os.Hostname()
+
+	// TODO get actual metrics
+	myMetrics := []influxdb.Metric{
+		influxdb.NewRowMetric(
+			map[string]interface{}{"memory": 1000, "cpu": 0.93},
+			"system-metrics",
+			map[string]string{"hostname": hostname},
+			time.Now().UTC()),
+	}
+
+	if err := influx.Write(context.Background(), "my-awesome-bucket", "my-awesome-org", myMetrics...); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func reportMetrics(influx *influxdb.Client, bucket string, org string, metrics []influxdb.Metric) error {
+	return influx.Write(context.Background(), bucket, org, metrics...)
 }
