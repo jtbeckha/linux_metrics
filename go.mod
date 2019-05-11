750fbfbc39739eef2de3e6c39df6b3aa5204f0e9
Jared <jared@jaredbeckham.com>
2019-05-11 16:31:35 -0400

    Add network stat parsing

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/jtbeckha/linux_metrics
 go 1.12
 
 require (
+	github.com/go-test/deep v1.0.1
 	github.com/influxdata/influxdb1-client v0.0.0-20190402204710-8ff2fc3824fc
 	gopkg.in/yaml.v2 v2.2.2
 )
--- network.go
+++ network.go
@@ -1,1 +1,87 @@
 package main
+
+import (
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
+const statsFile = "/proc/net/dev"
+
+func getNetworkMetrics() map[string]interface{} {
+	dataBytes, err := ioutil.ReadFile(statsFile)
+	if err != nil {
+		log.Println("Unable to open " + statsFile + ", network stats will not be available")
+		return nil
+	}
+
+	data := string(dataBytes)
+
+	return parseMetrics(data)
+}
+
+func parseMetrics(data string) map[string]interface{} {
+	lines := strings.Split(data, "\n")
+
+	// "receive", "transmit"
+	directions := strings.Split(lines[0], "|")[1:]
+	if len(directions) != 2 {
+		log.Println("Unexpected top-level header format encountered in " + statsFile)
+		return nil
+	}
+	for index, section := range directions {
+		directions[index] = strings.ToLower(strings.TrimSpace(section))
+	}
+
+	// "bytes", "packets", "errs", etc
+	metricLabelsPerDirection := strings.FieldsFunc(lines[1], func(r rune) bool {
+		return r == '|'
+	})[1:]
+	metricLabelsRx := strings.FieldsFunc(metricLabelsPerDirection[0], func(r rune) bool {
+		return r == ' '
+	})
+	metricLabelsTx := strings.FieldsFunc(metricLabelsPerDirection[1], func(r rune) bool {
+		return r == ' '
+	})
+
+	for index, section := range metricLabelsRx {
+		metricLabelsRx[index] = strings.ToLower(strings.TrimSpace(section))
+	}
+	for index, section := range metricLabelsTx {
+		metricLabelsTx[index] = strings.ToLower(strings.TrimSpace(section))
+	}
+
+	metrics := make(map[string]interface{})
+	for _, line := range lines[2:] {
+		if strings.TrimSpace(line) == "" {
+			continue
+		}
+
+		pieces := strings.FieldsFunc(line, func(r rune) bool {
+			return r == ' ' || r == ':'
+		})
+
+		interfaceName := pieces[0]
+		metricValues := pieces[1:]
+
+		for index, section := range metricValues {
+			metricValues[index] = strings.Trim(section, " ")
+		}
+
+		for index, value := range metricValues[:len(metricLabelsRx)] {
+			direction := directions[0]
+			label := metricLabelsRx[index]
+			metrics[interfaceName + "." + direction + "." + label], _ = strconv.Atoi(value)
+
+		}
+		for index, value := range metricValues[len(metricLabelsTx):] {
+			direction := directions[1]
+			label := metricLabelsTx[index]
+			metrics[interfaceName + "." + direction + "." + label], _ = strconv.Atoi(value)
+		}
+	}
+
+	return metrics
+}
+
